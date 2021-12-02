d56e61f8cc737de349ee9d27d2970caaba351f7c
madhanramesh <95333328+madhanramesh@users.noreply.github.com>
2021-12-02 16:05:07 +0530

    Initial commit
    new site

---
+++ First.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class First{ 
+
+	public static void main (String[] args)
+		// TODO Auto-generated method stub
+	{
+int number=200;
+for(int i=100; i<=number; i+=2) {
+	System.out.println(i);
+
+}
+}
+}
